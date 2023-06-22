0219a2affc0e33c2b767c17cde76535fccb83db0
ashwath007 <sanjanachandran1415@gmail.com>
2023-06-22 11:45:08 +0530

    working on: notification error

--- controllers/notification/notification.js
+++ controllers/notification/notification.js
@@ -2,11 +2,34 @@ const Pig = require("pigcolor");
 
 const nodemailer = require("nodemailer");
 const user = require("../../modules/user");
+const Notification = require("../../modules/notification");
+const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 
-const sendGmailToUser = async (userEmails) => {
+
+exports.getAllNotifications = (req, res) => {
+  Pig.box("GET: All Notification");
+  Notification.find({}).then((notify, err) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.json({
+      allNotifications: notify
+    })
+  }).catch((err) => {
+    return res.json({
+      error: err,
+    });
+    })
+}
+
+const sendGmailToUser = async (userEmails, makeSetLink, title) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    scope: 'https://mail.google.com',
+    secure: true,
     auth: {
       type: "OAuth2",
       user: "help.citi.quiz@gmail.com",
@@ -14,10 +37,13 @@ const sendGmailToUser = async (userEmails) => {
         "533381035374-imqb0tj97mpao8fqcv65g538pl3qhmtq.apps.googleusercontent.com",
       clientSecret: "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",
       refreshToken:
-        "1//04gvJHdwMzzrPCgYIARAAGAQSNwF-L9Irobjq_iXy0vC7G-IGyW1w7r-8GXWUMBLBRFCOdD57-HcVpIOX3hVIKA3elAQDIIcFkXo",
+        "1//04_7bubdZ5yl2CgYIARAAGAQSNwF-L9IrPYZHn00iakuwzX9eyMjXTjqhfFS8fH16LFnFbzbE1OWA5NT_E_4Q5AlmLGW5ldIQcbw",
       accessToken:
-        "ya29.a0AWY7CkkgO1C3g66oLOvzSELgve0pe_ADPE6hsgdY68fc-IyXH-fcKPaIVQKaNfkBn3VlqPDW0L14CCFBZUqYWHbu3dGHWcWF7je8eoyZ4MoBnXw0hDTGxsi0pPm9SlSyZfWKOg77oltTOX7R0ioyER4N27IHaCgYKAWUSARASFQG1tDrp6NM-Gq2IN206G9HV91wWgw0163",
+        "ya29.a0AWY7CknZstkk_lOJqA1J3vtFeMahocXgkb34FQ90WMpPCx9h3uRk4GnvIcMNp5sej5Fz91f-dD_noCrNLgOJI2VKpy8tj3_xzz7xhTaFEA603laYOYehLVCh2r22MhnMaP30FvfdHlSGDD9oF0lZlzKes1a1aCgYKARYSARASFQG1tDrpOuWUSRUSNJk9D5RMYszmsg0163",
     },
+    tls: {
+      rejectUnauthorized: false
+    }
   });
 
   const mailOption = {
@@ -25,7 +51,7 @@ const sendGmailToUser = async (userEmails) => {
     to: userEmails,
     subject: "Attend New Set",
     text: "citi-quiz.com new setr",
-    html: "<div><p><b>citi-quiz.com - New Set</b></p></div>",
+    html: `<div><p><b>citi-quiz.com - New Set</b><a href=${makeSetLink}>${title}</a></p></div>`,
   };
 
   try {
@@ -39,6 +65,9 @@ const sendGmailToUser = async (userEmails) => {
 exports.createNotification = async (req, res) => {
   Pig.box("Create Notification");
   console.log(req.body);
+
+  const makeSetLink = `http://localhost:8100/set/${req.body.set}/modes`;
+
   user
     .find({})
     .select("email")
@@ -53,7 +82,7 @@ exports.createNotification = async (req, res) => {
           msg: "User not found",
         });
       }
-      // console.log("All Emails - ", allNotify);
+      console.log("All Emails - ", allNotify);
       console.log("All Emails - ", allNotify);
       let allUserEmails = [];
       if (allNotify)
@@ -61,12 +90,32 @@ exports.createNotification = async (req, res) => {
           allUserEmails.push(email.email);
         });
       console.log("All Emails list - ", allUserEmails);
+      // Save Notification
+
+      const notification = Notification();
+      notification.notificationId = uuidv4();
+      notification.notificationTitle = req.body.title;
+      notification.notificationDescription = req.body.message;
+      notification.notificationLink = req.body.set;
       // Send email to all
-      const sendEmailRes = await sendGmailToUser(allUserEmails)
+      const sendEmailRes = await sendGmailToUser(allUserEmails,makeSetLink, req.body.title)
         .then((res) => {
           console.log("Email Send!!!", res);
-
-          return res.json({
+          notification.save().then((notify, err) => {
+            if(err){
+              return res.status(400).json({
+                error: err
+              })
+            }
+            return res.json({
+              notify: notify
+            })
+        }).catch((err) => {
+              return res.status(400).json({
+                error: err
+              })
+        });
+          return ({
             email: "All Done",
             to: allNotify,
           });
@@ -74,6 +123,8 @@ exports.createNotification = async (req, res) => {
         .catch((err) => {
           console.log(err);
         });
+    
+      
     })
     .catch((err) => {
       if (err) {
